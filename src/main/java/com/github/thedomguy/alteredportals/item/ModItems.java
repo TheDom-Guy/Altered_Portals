6f77f0effe819383687880016cc757267f083851
DominikTomczyk <tsylwia00@gmail.com>
2024-11-17 12:16:03 +0000

    Import cleanup

--- src/main/java/com/github/thedomguy/alteredportals/item/ModItems.java
+++ src/main/java/com/github/thedomguy/alteredportals/item/ModItems.java
@@ -3,15 +3,11 @@ package com.github.thedomguy.alteredportals.item;
 import com.github.thedomguy.alteredportals.AlteredPortals;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
-import net.minecraft.component.DataComponentTypes;
-import net.minecraft.component.type.ToolComponent;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
-
-import java.util.List;
 
 public class ModItems {
     public static final Item ALTER_TAIL = registerItem("alter_tail", new AlterTailItem(new Item.Settings().maxDamage(100)));
